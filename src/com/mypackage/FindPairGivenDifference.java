3e5af39de539912bfc16b2bee53a2c6a77c5b894
johnM66 <32862342+johnM66@users.noreply.github.com>
2022-03-29 23:21:00 +0530

    commit

--- src/com/mypackage/FindPairGivenDifference.java
+++ src/com/mypackage/FindPairGivenDifference.java
@@ -1,5 +1,21 @@
 package com.mypackage;
 
 public class FindPairGivenDifference {
+	public static void main(String[] args) {
+		int[] arr = { 1, 5, 2, 2, 2, 5, 5, 4 };
+		int diff = 3;
+
+		for (int i = 0; i < arr.length; i++) {
+			
+			for(int j=i+1;j<arr.length;j++) {
+				int rem=diff+arr[i];
+				if(arr[j]==rem) {
+					System.out.println("Index found at - "+i+","+j);
+					rem=0;
+					
+				}
+			}
+		}
+	}
 
 }
